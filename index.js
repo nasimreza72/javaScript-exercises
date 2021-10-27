2453fba9a8e6a1e3dd4059f236409a208559f3ea
Reza <nasimreza72>
2021-10-27 15:33:00 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,216 @@
+// console.log("Hello guys!");
+// // var name="Antonia";
+// let firstName="Tonia";
+// // console.log(name);
+// let nameSurname ="Antonia Athanasopoulou";
+// console.log(nameSurname, firstName);
+
+// //Rules for decalring variables 
+// //cannot be reserved keyword 
+// //should be meaningful 
+// // cannot start with number (let 1name="Antonia";) error 
+// //cannot containe a space or hyphen (-)
+// //Are case-sensitive
+
+// let numberA=0.5;
+// console.log(numberA);
+//  numberA=0.7;
+//  console.log(numberA);
+
+//  const numberB=0.2;
+//  console.log(numberB);
+// //  numberB=0.4;
+// //  console.log(numberB);
+
+//  let numberC=1, numberD=2, number=0;
+// //  numberC=numberA;
+// //  let numberC=numberA;
+//  console.log(numberC,numberD,number);
+
+//  //1.Primitive values
+//  //Boolean type
+//  //Number type
+//  //String type
+//  //Undefined
+//  //Null 
+// let orderApproved= true;
+// let age=30;
+// let studentName="Ashley";
+// let orders= undefined;
+// let color="red";
+// color=null;
+// console.log(color);
+
+
+//  //2.Objects (reference types) 
+//  //Array
+//  //Object
+//  //Function
+// let nameArray =["Antonia", "Ashley", "Latifah"];
+// let studentNames=[];
+// let gradeArray =[5,8,9];
+// console.log(nameArray[1]);
+// let car={brand:"BMW", year:2000, color:"black"};
+// console.log(car.color,car.brand);
+// // function()
+
+
+// //Arithmetic operations 
+// //1. Adding numbers 
+
+// let a=2;
+// let b=6;
+// let c=3;
+// let sum=a+b;
+// // let sum=1+1;
+// console.log(sum);
+// let sum2=50;
+// // sum2=sum2+1;
+
+// sum2++; //incrementing by 1 
+// console.log(sum2);
+// // console.log("The result is "+ sum2);
+
+// a+=12;  //a=a+12;
+// console.log("Value of a= ", a);
+// a-=6; //a=a-6;
+// console.log("Value of a= ", a);
+// a*=2; //a=a*2;
+// console.log("Value of a= ", a);
+// a/=2; // a=a/2;
+// console.log("Value of a= ", a);
+
+
+
+
+// //2. Substraction
+// let difference =30-10;
+// console.log(difference);
+
+// let difference2=10;
+// // difference2=difference2-1;
+// difference2--; //decrementing by 1
+// console.log(difference2);
+
+// //3. Multiplication
+// let product=8*3;
+// console.log(product);
+
+// //4.Division
+// let division=61/10;
+// console.log(division);
+
+// //5.Remainder
+// let remainder=60%10;
+// console.log(remainder);
+
+// exercise 1
+
+let x=6450;
+let y=15;
+let division=x/y;
+console.log(division);
+
+
+// exercise 2
+
+let sylbia=500;
+let salary=52*sylbia;
+console.log(salary);
+
+// exercise 3
+
+let a=17;
+let percentage=17*100;
+let b=30;
+let result=percentage/b;
+console.log(result)
+
+// exercise 4
+let square=4.75;
+let quotient=4*square;
+console.log(quotient);
+
+// exercise 5
+
+let perimeter=5+6+7;
+console.log(perimeter);
+
+// exercise 6
+
+let length=5;
+let width=5;
+console.log(length*width)
+
+// exercise 7
+
+let base=3;
+let height=4;
+let area=(3*4)/2;
+console.log(area);
+
+// exercise 8
+
+let side=9;
+let cubeArea=(9*9)*6;
+console.log(cubeArea);
+
+// exercise 9
+
+let bill1=22.35;
+let tip1=22.35*.1;
+let total1=bill1+tip1;
+console.log(total1);
+
+
+let bill2=26.67;
+let tip2=26.67*.15;
+let total2= bill2+tip2;
+console.log(total2);
+
+let bill3=35.92;
+let tip3=35.92*.2;
+let total3= bill3+tip3;
+console.log(total3);
+
+// exercise 10
+
+let totalHour=8+6+5+9+8+2+1+8.5+7+4;
+let average=totalHour/10;
+console.log("average",average)
+
+// exercise 11
+
+let test=(85*6)-420;
+console.log("test",test);
+
+
+// exercise 12
+
+let answer=96;
+console.log(answer);
+
+// exercise for today
+
+
+let myName="naSim"
+console.log(myName);
+
+let newName=myName.toLowerCase();
+console.log(newName);
+
+let updatedName=newName.substring(0,1);
+console.log(updatedName);
+
+let updatedName1=newName.substring(1,5);
+console.log(updatedName1);
+
+let updatedName2=updatedName.toUpperCase();
+console.log(updatedName2);
+
+console.log(updatedName2+updatedName1);
+
+
+
+
+
